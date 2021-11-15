729c29e4c16a26dfea135770face8d5328a5f6d5
frc6995students <93299011+frc6995students@users.noreply.github.com>
2021-11-14 17:32:30 -0800

    Root page id is wrong

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '5359641e17344c3fb2d57374094896f3',
+  rootNotionPageId: 'bd05296aa98f4244b83084803e29f712',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
